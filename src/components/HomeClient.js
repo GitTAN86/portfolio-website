d8cab557da8f9a55b32bd88b6a427fda24584b23
Bahman Noushabadi <bahman.noushabadii@gmail.com>
2026-05-14 03:50:41 +0800

    cleaning the removed pages

--- src/components/HomeClient.js
+++ src/components/HomeClient.js
@@ -1,5 +1,4 @@
 "use client";
-import { useState, useEffect } from "react";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
@@ -9,18 +8,7 @@ import FeedbackModal from "@/components/FeedbackModal";
 import AdminLink from "@/components/AdminLink";
 import ScrollThemeManager from "@/components/ScrollThemeManager";
 import ParticleBackground from "@/components/ParticleBackground";
-import GhostPaperCV from "@/components/GhostPaperCV";
-
 export default function HomeClient({ initialData }) {
-  const [isMobile, setIsMobile] = useState(true);
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    handleResize(); // Init
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <>
       <ScrollThemeManager />
@@ -28,16 +16,8 @@ export default function HomeClient({ initialData }) {
       <main className="content-wrapper">
         <Hero data={initialData} />
         <About data={initialData} />
-        
-        {!isMobile && <GhostPaperCV data={initialData} />}
-
-        {isMobile && (
-          <>
-            <Skills data={initialData} />
-            <Experience data={initialData} />
-          </>
-        )}
-        
+        <Skills data={initialData} />
+        <Experience data={initialData} />
         <Footer data={initialData} />
       </main>
       
